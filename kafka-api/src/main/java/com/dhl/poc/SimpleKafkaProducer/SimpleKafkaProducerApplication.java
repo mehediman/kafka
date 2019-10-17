5f70fbc76b05899b1ddefc430cc2301da54aa0c5
mehediman <mehedi@wayne.edu>
2019-10-17 04:50:42 -0400

    fixed bug for kafka version

--- kafka-api/src/main/java/com/dhl/poc/SimpleKafkaProducer/SimpleKafkaProducerApplication.java
+++ kafka-api/src/main/java/com/dhl/poc/SimpleKafkaProducer/SimpleKafkaProducerApplication.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 @SpringBootApplication
 public class SimpleKafkaProducerApplication implements CommandLineRunner {
 
-    @Value("${kafka.topic.thetechcheck}")
+    @Value("${kafka.topic}")
     private String theTechCheckTopicName;
 
     @Value("${kafka.bootstrap.servers}")
