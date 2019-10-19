a0a49dd05058fb7989da4401febbccf18f8b1754
mehedi-h <mehedi.h@samsung.com>
2019-10-18 18:17:30 -0700

    refactored application

--- kafka-api/src/main/java/com/dhl/kafka/SimpleKafkaAPIApplication.java
+++ kafka-api/src/main/java/com/dhl/kafka/SimpleKafkaAPIApplication.java
@@ -5,6 +5,7 @@ import com.dhl.kafka.producers.SimpleKafkaProducer;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -25,9 +26,6 @@ public class SimpleKafkaAPIApplication implements CommandLineRunner {
     @Value("${zookeeper.groupId}")
     private String zookeeperGroupId;
 
-    @Value("${zookeeper.host}")
-    String zookeeperHost;
-
     private static final String CONSUMER = "c";
     private static final String PRODUCER = "p";
 
@@ -43,53 +41,36 @@ public class SimpleKafkaAPIApplication implements CommandLineRunner {
         String process = (args.length > 1 && args[1].equalsIgnoreCase(CONSUMER)) ? CONSUMER : PRODUCER;
 
         if (process.equalsIgnoreCase(PRODUCER)) {
-            /*
-             * Defining producer properties.
-             */
+
             Properties producerProperties = new Properties();
-            producerProperties.put("bootstrap.servers", kafkaBootstrapServers);
+            producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
             producerProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
             producerProperties.put("ssl.truststore.location","/tmp/kafka.client.truststore.jks");
-            producerProperties.put("acks", "all");
-            producerProperties.put("retries", 0);
-            producerProperties.put("batch.size", 16384);
-            producerProperties.put("linger.ms", 1);
-            producerProperties.put("buffer.memory", 33554432);
-            producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");
+            producerProperties.put(ProducerConfig.RETRIES_CONFIG, 0);
+            producerProperties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+            producerProperties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+            producerProperties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+            producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+            producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        /*
-        Creating a Kafka Producer object with the configuration above.
-         */
             SimpleKafkaProducer kafkaProducer = new SimpleKafkaProducer(topicName, producerProperties);
             kafkaProducer.sendMessages();
         }
         else {
-            /*
-        Now that we've produced some test messages, let's see how to consume them using a Kafka consumer object.
-         */
 
-            /*
-             * Defining Kafka consumer properties.
-             */
             Properties consumerProperties = new Properties();
-            consumerProperties.put("bootstrap.servers", kafkaBootstrapServers);
-            consumerProperties.put("group.id", zookeeperGroupId);
+            consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
+            consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, zookeeperGroupId);
             consumerProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
             consumerProperties.put("ssl.truststore.location","/tmp/kafka.client.truststore.jks");
-            consumerProperties.put("zookeeper.session.timeout.ms", "6000");
-            consumerProperties.put("zookeeper.sync.time.ms","2000");
-            consumerProperties.put("auto.commit.enable", "true");
-            consumerProperties.put("auto.commit.interval.ms", "1000");
-            consumerProperties.put("consumer.timeout.ms", "-1");
-            consumerProperties.put("max.poll.records", "1");
-            consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-            consumerProperties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-            consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+            consumerProperties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+            //consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+            consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
-            /*
-             * Creating a thread to listen to the kafka topic
-             */
+
             Thread kafkaConsumerThread = new Thread(() -> {
                 logger.info("Starting Kafka consumer thread.");
 
@@ -101,9 +82,6 @@ public class SimpleKafkaAPIApplication implements CommandLineRunner {
                 simpleKafkaConsumer.runSingleWorker();
             });
 
-            /*
-             * Starting the first thread.
-             */
             kafkaConsumerThread.start();
         }
     }
--- kafka-api/src/main/java/com/dhl/kafka/consumers/SimpleKafkaConsumer.java
+++ kafka-api/src/main/java/com/dhl/kafka/consumers/SimpleKafkaConsumer.java
@@ -26,78 +26,35 @@ public class SimpleKafkaConsumer {
         kafkaConsumer.subscribe(topics);
     }
 
-    /**
-     * This function will start a single worker thread per topic.
-     * After creating the consumer object, we subscribed to a list of Kafka topics, in the constructor.
-     * For this example, the list consists of only one topic. But you can give it a try with multiple topics.
-     */
     public void runSingleWorker() {
+        logger.info("RECORD OFFSET RESET TO 0");
+        kafkaConsumer.seekToBeginning(kafkaConsumer.assignment());
 
-        /*
-         * We will start an infinite while loop, inside which we'll be listening to
-         * new messages in each topic that we've subscribed to.
-         */
         while(true) {
 
-            //kafkaConsumer.poll(Duration.ZERO);
-            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(10000));
 
             for (ConsumerRecord<String, String> record : records) {
 
-                /*
-                Whenever there's a new message in the Kafka topic, we'll get the message in this loop, as
-                the record object.
-                 */
-
-                /*
-                Getting the message as a string from the record object.
-                 */
                 String message = record.value();
-
-                /*
-                Logging the received message to the console.
-                 */
                 logger.info("Received message: " + message);
 
-                /*
-                If you remember, we sent 10 messages to this topic as plain strings.
-                10 other messages were serialized JSON objects. Now we'll deserialize them here.
-                But since we can't make out which message is a serialized JSON object and which isn't,
-                we'll try to deserialize all of them.
-                So, obviously, we'll get an exception for the first 10 messages we receive.
-                We'll just log the errors and not worry about them.
-                 */
                 try {
                     JSONObject receivedJsonObject = new JSONObject(message);
-
-                    /*
-                    To make sure we successfully deserialized the message to a JSON object, we'll
-                    log the index of JSON object.
-                     */
                     logger.info("Index of deserialized JSON object: " + receivedJsonObject.getInt("index"));
                 } catch (JSONException e) {
                     logger.error(e.getMessage());
                 }
 
-                /*
-                Once we finish processing a Kafka message, we have to commit the offset so that
-                we don't end up consuming the same message endlessly. By default, the consumer object takes
-                care of this. But to demonstrate how it can be done, we have turned this default behaviour off,
-                instead, we're going to manually commit the offsets.
-                The code for this is below. It's pretty much self explanatory.
-                 */
-                /*{
+                {
                     Map<TopicPartition, OffsetAndMetadata> commitMessage = new HashMap<>();
-
                     commitMessage.put(new TopicPartition(record.topic(), record.partition()),
                             new OffsetAndMetadata(record.offset() + 1));
 
                     kafkaConsumer.commitSync(commitMessage);
-
                     logger.info("Offset committed to Kafka.");
                     logger.info("Record offset: " + record.offset());
-                }*/
-                logger.info("Record offset: " + record.offset());
+                }
             }
         }
     }
